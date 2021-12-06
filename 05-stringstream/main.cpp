f0056b02fa0c2d5595af02137aea25f08696a477
jwbat <jwesleyb@gmail.com>
2021-12-06 09:31:01 -0800

    .

--- 05-stringstream/main.cpp
+++ 05-stringstream/main.cpp
@@ -4,13 +4,23 @@
 #include <sstream>
 
 using std::cout; using std::setw; using std::string; using std::stringstream;
+void nl(int n = 1) { for (size_t i{ 0 }; i < n; ++i) cout << "\n"; }
+void tab(int n = 1) { for (size_t i{ 0 }; i < n; ++i) cout << "\t"; }
+void print(auto x) { cout << x; }
 
 int main() {
 
-    stringstream strs;
-    strs << " x " << 9 << " x " << 9;
-    string s = strs.str();
-    cout << "\n" << s << "\n\n";
+    stringstream stream;
+    stream << "x " << 9 << " x " << 9 << "\n\n";
+    string s1 = stream.str();
+    nl(); tab(2);
+    print(s1);
+    nl();
 
+    //.............................................................
+    string s2{ "999" };
+    int x;
+    stringstream(s2) >> x;
+    nl(2); tab(2); print(x); nl(2);
     return 0;
 }
