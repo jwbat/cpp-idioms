62672091bd3066b2f7e8ec27be408ce15e815675
jwbat <jwesleyb@gmail.com>
2021-12-06 19:03:45 -0800

    .

---
+++ 07-constexpr/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <vector>
+
+using std::cout; using std::setw; using std::string;
+using std::vector; using std::memset;
+
+constexpr void newline(int n = 1) { for (size_t i{ 0 }; i < n; ++i) cout << "\n"; }
+constexpr void tab(int n = 1) { for (size_t i{ 0 }; i < n; ++i) cout << "\t"; }
+template <typename T> constexpr void print(T x) { cout << x; }
+
+constexpr int add(int a, int b) { return a + b; }
+
+void fast_zero(vector<int>& v) {
+    memset(v.data(), 0, v.size() * sizeof(int));
+}
+
+int main() {
+    newline(); tab(); print(add(5, 7));
+
+
+
+    vector<int> xs(10); fast_zero(xs); newline(2);
+    for (auto n : xs)
+        cout << setw(5) << n;
+    newline(2);
+    return 0;
+}
