6a6e62b323e1475159c15cccdf4f9c8c8f7cefe9
jwbat <jwesleyb@gmail.com>
2021-12-10 08:03:11 -0800

    .

--- 08-map/main.cpp
+++ 08-map/main.cpp
@@ -9,25 +9,25 @@ using std::map; using std::tie; using std::boolalpha;
 void map_insert(map<int, string>& m, int i, string s);
 
 int main() {
-
     map<int, string> m;
     map_insert(m, 1, "one");
     map_insert(m, 2, "two");
     map_insert(m, 3, "three");
     map_insert(m, 1, "one");
 
-
-
     return 0;
 }
 
 void map_insert(map<int, string>& m, int i, string s) {
-    map<int, string>::iterator it;
-    bool inserted;
-    tie(it, inserted) = m.insert({ i, s });
-    cout << boolalpha 
-         << "\n\t inserted = " << setw(10) << inserted
-         << "\n\t key      = " << setw(10) << it->first
-         << "\n\t value    = " << setw(10) << it->second
-         << "\n\n";
+    if (auto [it, inserted] = m.insert({ i, s }); inserted) {
+        cout << boolalpha 
+             << "\n\t inserted = " << setw(10) << inserted
+             << "\n\t key      = " << setw(10) << it->first
+             << "\n\t value    = " << setw(10) << it->second
+             << "\n\n";
+    }
 }
+
+//    map<int, string>::iterator it;
+//    bool inserted;
+//    tie(it, inserted) = m.insert({ i, s });
