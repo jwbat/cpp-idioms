9b3b65cd6b88a577254f1666496a550186bd2941
jwbat <jwesleyb@gmail.com>
2021-12-12 10:33:05 -0800

    .

--- 13-regex/main.cpp
+++ 13-regex/main.cpp
@@ -8,7 +8,7 @@ using namespace std::string_literals; using std::boolalpha;
 using std::regex; using std::regex_constants::icase; using std::regex_match;
 
 // match valid email addresses
-constinit auto pattern {R"(^[A-Z0-9.%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$)"s};
+auto pattern {R"(^[A-Z0-9.%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$)"s};
 auto rx = regex{ pattern, icase };
 
 int main()
