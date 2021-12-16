72bb62f0163643eeab7223e2c1cdf586bfae9ea0
James <jwesleyb@gmail.com>
2021-12-16 15:29:18 -0800

    .

---
+++ 17-fold/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <numeric>
+
+using std::cout; using std::setw; using std::forward; using std::move;
+using std::vector; using std::accumulate;
+
+template <typename F, typename R, typename T>
+constexpr T foldl(F&& f, R&& range, T init)
+{
+    return accumulate(begin(range), end(range), move(init), forward<F>(f));
+}
+
+constexpr auto mult = [] (const auto a, const auto b) { return a * b; };
+
+int main()
+{
+    vector<int> v{ 4, 5, 20 };
+    int sum = foldl(mult, v, 1);        // 400
+    cout << "\n\t" << sum << "\n\n";
+
+    return 0;
+}
