77b787b8421215f5385e615a8933442c7910ca99
James <jwesleyb@gmail.com>
2021-12-17 13:16:07 -0800

    .

--- 13-regex/main.cpp
+++ 13-regex/main.cpp
@@ -7,8 +7,9 @@ using std::cout; using std::setw; using std::string;
 using namespace std::string_literals; using std::boolalpha;
 using std::regex; using std::regex_constants::icase; using std::regex_match;
 
-// match valid email addresses
-
+/*
+ * match valid email addresses
+*/
 bool is_valid_email_format(const string& email)
 {
     auto pattern {R"(^[A-Z0-9.%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$)"s};
