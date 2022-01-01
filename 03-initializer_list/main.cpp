0f5928509d5e9baa48c8dc1c2241758b4f074b8f
jwbat <jwesleyb@gmail.com>
2022-01-01 13:21:13 -0800

    .

--- 03-initializer_list/main.cpp
+++ 03-initializer_list/main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <cmath>
 
-using std::cout; using std::string; using std::setw; using std::setprecision;
-using std::initializer_list; using std::fixed;
-
-void print_large_int(const long double& x);
+using std::cout; using std::setw; using std::initializer_list;
 
 struct A {
-    static const int n{ 1000 };
     A(const initializer_list<int>& lst) {
         cout << "\n";
         for (auto n : lst) {
@@ -23,14 +18,5 @@ int main() {
 
     A a{ 1, 2, 3 };
 
-    cout << "\t" << a.n << "\n\n";
-
-    print_large_int(pow(999, 99));
-
     return 0;
-}
-
-void print_large_int(const long double& x){
-    cout << fixed << setprecision(0) << "\t" << x
-         <<  "\n\n";
 }
